1d1e8b4f44a9d8850ba308dcbf1f519a513568d3
Pavel Kliukin <9213087@mail.ru>
2022-04-15 00:00:39 +0300

    Removed all unnecessary

--- SplayTreeTraining.py
+++ SplayTreeTraining.py
@@ -1,181 +1,3 @@
-import matplotlib.pyplot as plt
-import networkx as nx
-
-
-def _get_pos_list(tree):
-    """
-    _get_pos_list(tree) -> Mapping. Produces a mapping
-    of nodes as keys, and their coordinates for plotting
-    as values. Since pyplot or networkx don't have built in
-    methods for plotting binary search trees, this somewhat
-    choppy method has to be used.
-    """
-    return _get_pos_list_from(tree,tree.Root,{},0,(0,0),1.0)
-
-def _get_pos_list_from(tree,node,poslst,index,coords,gap):
-    """
-    _get_pos_list_from(tree,node,poslst,index,coords,gap) -> Mapping.
-    Produces a mapping of nodes as keys, and their coordinates for
-    plotting as values.
-    Non-straightforward arguments:
-    index: represents the index of node in
-    a list of all Nodes in tree in preorder.
-    coords: represents coordinates of node's parent. Used to
-    determine coordinates of node for plotting.
-    gap: represents horizontal distance from node and node's parent.
-    To achieve plotting consistency each time we move down the tree
-    we half this value.
-    """
-    positions = poslst
-
-    if node and node.key == tree.Root.key:
-        positions[0] = (0,0)
-        positions = _get_pos_list_from(tree,tree.Root.left,positions,1,(0,0),gap)
-        positions = _get_pos_list_from(tree,tree.Root.right,positions,1+tree.get_element_count(node.left),(0,0),gap)
-        return positions
-    elif node:
-        if node.parent.right and node.parent.right.key == node.key:
-            new_coords = (coords[0]+gap,coords[1]-1)
-            positions[index] = new_coords
-        else:
-            new_coords = (coords[0]-gap,coords[1]-1)
-            positions[index] = new_coords
-
-        positions = _get_pos_list_from(tree,node.left,positions,index+1,new_coords,gap/2)
-        positions = _get_pos_list_from(tree,node.right,positions,1+ index + tree.get_element_count(node.left), new_coords,gap/2)
-        return positions
-    else:
-        return positions
-
-def _get_edge_list(tree):
-    """
-    _get_edge_list(tree) -> Sequence. Produces a sequence
-    of tuples representing edges to be drawn.
-    """
-    return _get_edge_list_from(tree,tree.Root,[],0)
-
-def _get_edge_list_from(tree,node,edgelst,index):
-    """
-    _get_edge_list_from(tree,node,edgelst,index) -> Sequence.
-    Produces a sequence of tuples representing edges to be drawn.
-    As stated before, index represents the index of node in
-    a list of all Nodes in tree in preorder.
-    """
-    edges = edgelst
-
-    if node and node.key == tree.Root.key:
-        new_index = 1 + tree.get_element_count(node.left)
-
-        if node.left:
-            edges.append((0,1))
-            edges = _get_edge_list_from(tree,node.left,edges,1)
-        if node.right:
-            edges.append((0,new_index))
-            edges = _get_edge_list_from(tree,node.right,edges,new_index)
-
-        return edges
-
-    elif node:
-        new_index = 1 + index + tree.get_element_count(node.left)
-
-        if node.left:
-            edges.append((index,index+1))
-        if node.right:
-            edges.append((index,new_index))
-
-        edges = _get_edge_list_from(tree,node.left,edges,index+1)
-        edges = _get_edge_list_from(tree,node.right,edges,new_index)
-        return edges
-
-    else:
-        return edges
-
-def _preorder(tree,*args):
-    """
-    _preorder(tree,...) -> Sequence. Produces a sequence of the Nodes
-    in tree, obtained in preorder. Used to get information
-    for plotting.
-    """
-    if len(args) == 0:
-        elements = []
-        node = tree.Root
-    else:
-        node = tree
-        elements = args[0]
-
-    elements.append(node)
-
-    if node.left:
-        _preorder(node.left,elements)
-    if node.right:
-        _preorder(node.right,elements)
-
-    return elements
-
-def _get_label_list(tree):
-    """
-    _get_pos_list(tree) -> Mapping. Produces a mapping
-    of nodes as keys, and their labels for plotting
-    as values.
-    """
-    nodelist = _preorder(tree)
-    labellist = {}
-    index = 0
-    for node in nodelist:
-        labellist[index] = node.key
-        index = index + 1
-
-    return labellist
-
-def _get_color_list(tree):
-    """
-    _get_color_list(tree) -> Sequence. Produces
-    a sequence of colors in tree for plotting.
-    NOTE: Assumes tree is a Red Black Tree.
-    This is checked first in the main function draw().
-    """
-    nodelist = _preorder(tree)
-    colorlist = []
-    for node in nodelist:
-        if node.color:
-            colorlist.append(node.color)
-
-    return colorlist
-
-def plot_tree(tree):
-    """
-    plot_tree(tree). Utilizes networkx and the methods above
-    to create a graph to represent a binary search tree, and
-    then utilizes pyplot to draw the tree to the screen.
-    """
-    G=nx.Graph()
-
-    pos = _get_pos_list(tree)
-    nodes = [x for x in pos.keys()]
-    edges = _get_edge_list(tree)
-    labels = _get_label_list(tree)
-    colors = []
-    try:
-        colors = _get_color_list(tree)
-    except AttributeError:
-        pass
-
-    G.add_edges_from(edges)
-    G.add_nodes_from(nodes)
-
-    if len(colors) > 0:
-        nx.draw_networkx_nodes(G,pos,node_size=400,node_color=colors)
-        nx.draw_networkx_edges(G,pos)
-        nx.draw_networkx_labels(G,pos,labels,font_color='w')
-    else:
-        nx.draw_networkx_nodes(G,pos,node_size=400,node_color='r')
-        nx.draw_networkx_edges(G,pos)
-        nx.draw_networkx_labels(G,pos,labels)
-
-    plt.axis('off')
-    plt.show()
-
-
 import collections
 import collections.abc
 
@@ -212,58 +34,6 @@ class BSTree:
             else:
                 raise TypeError(str(args[0]) + " is not iterable")
 
-    def is_valid(self, *args):
-        """
-        T.is_valid(...) -> Boolean. Produces True if and only if
-        T is a valid Binary Search Tree. Raises an exception otherwise.
-        """
-        if len(args) == 0:
-            node = self.Root
-        else:
-            node = args[0]
-
-        if not node:
-            return True
-
-        if node.left:
-            if not node.left.parent == node:
-                raise Exception("Left child of node " + str(node.key) + " is adopted by another node!")
-
-        if node.right:
-            if not node.right.parent == node:
-                raise Exception("Right child of node " + str(node.key) + " is adopted by another node!")
-
-        if node.parent and node.parent.left == node:
-            if node.key > node.parent.key:
-                raise Exception("Node " + str(node.key) + " is to the left of " + str(node.parent.key) + " but is larger")
-
-        if node.parent and node.parent.right == node:
-            if node.key < node.parent.key:
-                raise Exception("Node " + str(node.key) + " is to the right of " + str(node.parent.key) + " but is smaller")
-
-        return (self.is_valid(node.left) and self.is_valid(node.right))
-
-    def preorder(self,*args):
-        """
-        T.preorder(...) -> Sequence. Produces a sequence of the Nodes
-        in T, obtained in preorder.
-        """
-        if len(args) == 0:
-            elements = []
-            node = self.Root
-        else:
-            node = args[0]
-            elements = args[1]
-
-        elements.append(node)
-
-        if node.left:
-            self.preorder(node.left,elements)
-        if node.right:
-            self.preorder(node.right,elements)
-
-        return elements
-
     def inorder(self,*args):
         """
         T.inorder(...) -> Sequence. Produces a sequence of the Nodes
@@ -285,47 +55,6 @@ class BSTree:
             self.inorder(node.right,elements)
 
         return elements
-
-    def postorder(self,*args):
-        """
-        T.postorder(...) -> Sequence. Produces a sequence of the Nodes
-        in T, obtained in postorder.
-        """
-        if len(args) == 0:
-            elements = []
-            node = self.Root
-        else:
-            node = args[0]
-            elements = args[1]
-
-        if node.left:
-            self.postorder(node.left,elements)
-
-        if node.right:
-            self.postorder(node.right,elements)
-
-        elements.append(node)
-
-        return elements
-
-    def levelorder(self):
-        """
-        T.levelorder(...) -> Sequence. Produces a sequence of the Nodes
-        in T, obtained in levelorder.
-        """
-        q = collections.deque()
-        q.appendleft(self.Root)
-        lst = []
-        while len(q) != 0:
-            removed = q.pop()
-            lst.append(removed)
-            visit = self.get_node(removed,self.Root)
-            if visit.left:
-                q.appendleft(visit.left)
-            if visit.right:
-                q.appendleft(visit.right)
-
-        return lst
 
     def get_node(self,key,*args):
         """
@@ -576,17 +305,6 @@ class BSTree:
             else:
                 self._delete_node(node)
 
-    def delete_from(self,seq):
-        """
-        T.delete_from(seq). For every keyin seq, deletes
-        the Node with that key attribute from T.
-        """
-        if isinstance(seq,collections.Iterable):
-            for x in seq:
-                self.delete(x)
-        else:
-            raise TypeError(str(iter) + " is not iterable")
-
 
 class AVLNode(Node):
     """Represents a node of a balanced AVL Tree"""
@@ -657,41 +375,6 @@ class AVLTree(BSTree):
 
         return (self.is_valid(node.left) and self.is_valid(node.right))
 
-    def preorder(self,*args):
-        """
-        T.preorder(...) -> Sequence. Produces a sequence of the Nodes
-        in T, obtained in preorder.
-        """
-        return BSTree.preorder(self,*args)
-
-    def inorder(self,*args):
-        """
-        T.inorder(...) -> Sequence. Produces a sequence of the Nodes
-        in T, obtained in inorder.
-        """
-        return BSTree.inorder(self,*args)
-
-    def postorder(self,*args):
-        """
-        T.postorder(...) -> Sequence. Produces a sequence of the Nodes
-        in T, obtained in postorder.
-        """
-        return BSTree.postorder(self,*args)
-
-    def levelorder(self):
-        """
-        T.levelorder(...) -> Sequence. Produces a sequence of the Nodes
-        in T, obtained in levelorder.
-        """
-        return BSTree.levelorder(self,*args)
-
-    def get_node(self,key,*args):
-        """
-        T.get_node(key,...) -> Node. Produces the Node in T with key
-        attribute key. If there is no such Node, produces None.
-        """
-        return BSTree.get_node(self,key,*args)
-
     def insert(self,key,value,*args):
         """
         T.insert(key,value...) <==> T[key] = value. Inserts
@@ -737,42 +420,6 @@ class AVLTree(BSTree):
                             self._balance(node)
                     else:
                         self.insert(key,value,parent.left)
-
-    def insert_from(self,seq):
-        """
-        T.insert_from(seq). For every key, value pair in seq,
-        inserts a new Node into T with key and value attributes
-        as given.
-        """
-        BSTree.insert_from(self,seq)
-
-    def get_max(self,*args):
-        """
-        T.get_max(...) -> Node. Produces the Node that has the maximum
-        key attribute in T.
-        """
-        return BSTree.get_max(self,*args)
-
-    def get_min(self,*args):
-        """
-        T.get_min(...) -> Node. Produces the Node that has the minimum
-        key attribute in T.
-        """
-        return BSTree.get_min(self,*args)
-
-    def get_element_count(self,*args):
-        """
-        T.get_element_count(...) -> Nat. Produces the number of elements
-        in T.
-        """
-        return BSTree.get_element_count(self,*args)
-
-    def get_height(self,*args):
-        """
-        T.get_height(...) -> Nat. Produces the height of T, defined
-        as one added to the height of the tallest subtree.
-        """
-        return BSTree.get_height(self,*args)
 
     def get_balance(self,*args):
         """
@@ -830,7 +477,7 @@ class AVLTree(BSTree):
         temp = new_root.right
         old_root.right = new_root.left
 
-        if (old_root.right):
+        if old_root.right:
             old_root.right.parent = old_root
         new_root.left = old_root
         old_root.parent = new_root
@@ -1058,8 +705,7 @@ with open('test.txt') as f:
         if a == 's':
             s_new = 0
             c = (int(c[0]) + s) % 1000000001
-            A = T.preorder()
-            for elem in A:
+            for elem in T.inorder():
                 if elem.key in range(b, c + 1):
                     s_new += elem.key
             if s_new != 0:
